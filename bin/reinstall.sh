3109e5044dc3ef9ac5d6d7d112872f9d595b194f
Francis Araujo <francis.araujo@gmail.com>
2023-06-14 16:55:36 -0300

    Updates to use node selector and to not use master nodes storage

--- bin/reinstall.sh
+++ bin/reinstall.sh
@@ -6,6 +6,10 @@ cd ../k3s-ansible
 ./reinstall.sh
 cd -
 
+printf "Setting nodes with slave labels...\n"
+printf "#######################################################\n\n"
+./bin/setNodesSlaveLabel.sh
+
 printf "Installing flux env into k3s cluster...\n"
 printf "#######################################################\n\n"
 ./bin/install.sh
